ce216dddf4fe503f74504cf6adfa08b18a060355
Sameer <mohdsameer1313@gmail.com>
2024-02-10 00:02:43 +0530

    day1

---
+++ Login/src/test/java/Login.java
@@ -0,0 +1,20 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds("20"));
+		driver.get("https://rahulshettyacademy.com/client/");
+        driver.findElement(By.id("userEmail")).sendKeys("abdcdefgh@ijkl");
+        driver.findElement(By.id("userPassword")).sendKeys("almkhopst");
+     
+	}
+
+}
